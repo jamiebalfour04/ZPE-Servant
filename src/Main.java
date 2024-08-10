a80a58d5a9cadac9e5dc61589e7f707b481b50a7
Jamie Balfour <jamie@balfour.scot>
2024-08-10 09:52:04 +0100

    Initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+
+    Socket kkSocket = new Socket("localhost", 9342);
+    PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+
+    if(args.length > 0){
+      File f = new File(args[0]);
+      if(f.exists()){
+        out.write("{\"file\" : \""+f.getAbsolutePath()+"\"}");
+      }
+    }
+    String line;
+    while((line = in.readLine()) != null){
+      System.out.println(line);
+      kkSocket.close();
+    }
+
+
   }
 }
