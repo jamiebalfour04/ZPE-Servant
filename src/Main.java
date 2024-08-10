3d45cb06e788174b7434195c2e220fbe8f4d3b8b
Jamie Balfour <jamie@balfour.scot>
2024-08-10 10:48:44 +0100

    The Servant works with the ZPE -l argument. Run Servant with the file name of the executable and ZPE will run it, passing the result back to the Servant.

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,28 @@ public class Main {
     PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
     BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
 
-    if(args.length > 0){
-      File f = new File(args[0]);
-      if(f.exists()){
-        out.write("{\"file\" : \""+f.getAbsolutePath()+"\"}");
+    String line;
+    line = in.readLine();
+    if(line.equals("Ready")){
+      if(args.length > 0){
+        File f = new File(args[0]);
+        if(f.exists()){
+          out.write("{\"file\" : \""+f.getAbsolutePath()+"\"}" + '\n');
+        }
+        out.flush();
+
+        line = in.readLine();
+
+        //Output everything from the daemon
+        do {
+          System.out.println(line);
+        } while ((line = in.readLine()) != null);
+
+
+        kkSocket.close();
       }
     }
-    String line;
-    while((line = in.readLine()) != null){
-      System.out.println(line);
-      kkSocket.close();
-    }
+
 
 
   }
